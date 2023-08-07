0112be1992f0e18c028ec95482bb90ac7abefaa5
Vincent <vshan0816@gmail.com>
2023-08-07 15:15:43 -0400

    finish find_by_credentials

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -2,11 +2,35 @@ class User < ApplicationRecord
     validates :username, :session_token, presence: true, uniqueness: true 
     validates :password_digest, presence: true 
 
-    
+    before_validation :ensure_session_token
+
+    attr_reader: password
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        pass_object = BCrypt::Password.new(self.password_digest)
+        pass_object.is_password?(password)
+    end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by({username: username})
+
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
 
 
-
-
+    end
 
 
 end
